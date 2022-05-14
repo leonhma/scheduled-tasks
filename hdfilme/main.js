0366a43ba5b0da2bc608bf10653263e417642593
leonhma <67312077+leonhma@users.noreply.github.com>
2022-05-14 10:24:40 +0200

    only log coupon code

--- hdfilme/main.js
+++ hdfilme/main.js
@@ -21,7 +21,6 @@ const browser = await pptr.launch({ args: ['--no-sandbox'] });
 const context = browser.defaultBrowserContext();
 await context.overridePermissions('https://putput.net/', ['clipboard-read'])
 
-const hdfilme = await browser.newPage();
 const putput = await browser.newPage();
 
 await putput.goto('https://putput.net/lustig', { 'referer': 'https://www.google.com/' });
@@ -35,20 +34,9 @@ await uploadScreenshot(putput)
 await putput.click('#core-view > div > div.pp-section-list-container > div.gift-box > div > div > div.v-responsive__content')
 await uploadScreenshot(putput)
 console.log('waiting 120 secs')
-await Promise.all([
-  putput.waitForTimeout(125000),
-  (async function () {
-    await hdfilme.goto('https://hdfilme.tv/login/');
-    await hdfilme.waitForTimeout(10000);
-    await uploadScreenshot(hdfilme)
-    await hdfilme.type('#email', process.env.HDFILME_USER_EMAIL);
-    await hdfilme.type('#password', process.env.HDFILME_USER_PWD);
-    await uploadScreenshot(hdfilme)
-    await hdfilme.click('button[type=submit]')
-    await hdfilme.waitForNavigation({ waitUntil: 'networkidle0' })
-  }())])
+
+await putput.waitForTimeout(125000),
                   
-await uploadScreenshot(hdfilme)
 await uploadScreenshot(putput)
 
 await putput.click('#core-view > div > div.pp-section-list-container > div.gift-box > div > div.gift-box-body > div > div.gift-box-types > div:nth-child(2) > div.v-image.v-responsive.theme--light > div.v-responsive__content')
@@ -64,14 +52,4 @@ await uploadScreenshot(putput)
 
 const couponCode = await page.evaluate(`(async () => await navigator.clipboard.readText())()`)
 
-await hdfilme.goto('https://hdfilme.tv/submit-coupon/')
-await hdfilme.type('input#coupon_code', couponCode)
-
-await uploadScreenshot(hdfilme)
-
-await hdfilme.click('button[type=submit]')
-
-await uploadScreenshot(hdfilme)
-
-await hdfilme.waitForTimeout(2000)
-
+console.log(`coupon code: ${couponCode}`)
