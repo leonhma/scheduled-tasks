a9af1586835b110afee63e78f0a267cbcbdf91cd
leonhma <67312077+leonhma@users.noreply.github.com>
2022-05-14 14:31:14 +0200

    couponToken undefined

--- hdfilme/main.js
+++ hdfilme/main.js
@@ -18,16 +18,17 @@ const generateCoupon = async () => {
         "exp": 9999999999
     },
         'hoan_an_cac', { 'algorithm': 'HS256' });
-    return await page.evaluate(_ => {
+    return await page.evaluate((couponToken) => {
         fetch("https://api.putput.net/api/shared/claim-coupon",
             {
                 'method': 'POST',
                 'headers': {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
                 },
-                'body': JSON.stringify({ couponToken: couponToken })
+                'body': JSON.stringify({ 'couponToken': couponToken })
             }).then(res => res.json())
-    });
+    }, couponToken);
 }
 
 console.log(await generateCoupon())
