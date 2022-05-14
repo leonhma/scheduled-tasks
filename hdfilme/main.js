29f093819af02532c4dd536d2fc3a3470f860b65
leonhma <67312077+leonhma@users.noreply.github.com>
2022-05-14 23:58:59 +0200

    new Promise

--- hdfilme/main.js
+++ hdfilme/main.js
@@ -23,7 +23,7 @@ const generateCoupon = async () => {
         req.open('POST', 'https://api.putput.net/api/shared/claim-coupon')
         req.setRequestHeader('Content-Type', 'application/json');
         req.setRequestHeader('Accept', 'application/json');
-        return Promise((resolve, _reject) => { 
+        return new Promise((resolve, _reject) => { 
             req.onload = (res) => {
                 resolve(res)
             }
