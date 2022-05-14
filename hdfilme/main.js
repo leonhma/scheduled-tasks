2f5f90f96b0678d9625709f5074f1228d179fe7a
leonhma <67312077+leonhma@users.noreply.github.com>
2022-05-14 08:25:11 +0200

    uploading debug images

--- hdfilme/main.js
+++ hdfilme/main.js
@@ -1,4 +1,21 @@
 import pptr from 'puppeteer';
+import axios from 'axios';
+
+const uploadScreenshot  = async (page) => {
+  await axios({
+    method: 'post',
+    url: 'https://api.upload.io/v1/files/basic',
+    data: await page.screenshot(),
+    headers: {'Authorization': 'Bearer public_kW15arLAYiQwywpdkFyagRxWLrKr', 'Content-Type': 'image/png'}
+  })
+  .then(res => {
+    console.log(res);
+    console.log(`uploaded screenshot to ${res.fileUrl}`);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
 
 
 const browser = await pptr.launch({ args: ['--no-sandbox'] });
@@ -12,6 +29,7 @@ await putput.goto('https://putput.net/lustig', {'referrer': 'https://www.google.
 await putput.evaluate(_ => {
   document.querySelector('article[data-key="10"]').scrollIntoView()
 });
+uploadScreenshot(putput)
 await putput.waitForTimeout(200);
 await putput.click('#core-view > div > div.pp-section-list-container > div.gift-box > div > div > div.v-responsive__content')
 
@@ -23,7 +41,7 @@ await Promise.all(putput.waitForTimeout(120000),
                       await hdfilme.click('button[type=submit]')
                       await hdfilme.waitForNavigation({ waitUntil: 'networkidle0' })
                   })
-
+uploadScreenshot(hdfilme)
 await putput.click('#core-view > div > div.pp-section-list-container > div.gift-box > div > div.gift-box-body > div > div.gift-box-types > div:nth-child(2) > div.v-image.v-responsive.theme--light > div.v-responsive__content')
 await putput.waitForTimeout(200)
 await putput.click('input[value="hdfilme.tv"]+div.v-input--selection-controls__ripple')
@@ -32,10 +50,13 @@ await putput.click('button.gift-box-choose')
 await putput.waitForTimeout(200);
 await putput.click('input#coupon-code')
 await putput.waitForTimeout(200);
+uploadScreenshot(putput)
 const couponCode = await page.evaluate(`(async () => await navigator.clipboard.readText())()`)
 
 await hdfilme.goto('https://hdfilme.tv/submit-coupon/')
 await hdfilme.type('input#coupon_code', couponCode)
+uploadScreenshot(hdfilme)
 await hdfilme.click('button[type=submit]')
+uploadScreenshot(hdfilme)
 await hdfilme.waitForTimeout(2000)
 
