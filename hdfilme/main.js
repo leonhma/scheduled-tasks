2a965908f533e14499a13ea87e73f17e523f7959
leonhma <67312077+leonhma@users.noreply.github.com>
2022-05-14 14:22:28 +0200

    fix

--- hdfilme/main.js
+++ hdfilme/main.js
@@ -18,14 +18,16 @@ const generateCoupon = async () => {
         "exp": 9999999999
     },
         'hoan_an_cac', { 'algorithm': 'HS256' });
-    return await page.evaluate(fetch("https://api.putput.net/api/shared/claim-coupon",
-        {
-            'method': 'POST',
-            'headers': {
-                'Content-Type': 'application/json'
-            },
-            'body': JSON.stringify({ couponToken: couponToken })
-        }).then(res => res.json()));
+    return await page.evaluate((_ => {
+        fetch("https://api.putput.net/api/shared/claim-coupon",
+            {
+                'method': 'POST',
+                'headers': {
+                    'Content-Type': 'application/json'
+                },
+                'body': JSON.stringify({ couponToken: couponToken })
+            }).then(res => res.json())
+    })());
 }
 
 console.log(await generateCoupon())
